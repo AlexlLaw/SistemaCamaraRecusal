180e5cb8fdf06c6876300ec3f471514436480f59
NaoTenhoNome <eydenig66345@gmail.com>
2020-02-11 13:23:34 -0300

    adicionando padrão de projeto do codigo

--- procedimentos/processos/eliminarProcessos2.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "../../classes/conexao.php";
-require_once "../../classes/processos2.php";
-$id = $_POST['idprocesso2'];
-$obj = new processos2();
-echo $obj->excluir($id);
--- procedimentos/processos/obterDadosProcessos2.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "../../classes/conexao.php";
-require_once "../../classes/processos2.php";
-$obj = new processos2();
- //a função obterDados está na url ../classes/processos na linha 24. Via método JSON os dados estão sendo enviados para view/marco.php no método adiconarDados da linha 109
-echo json_encode($obj->obterDados($_POST['idprocesso2']));
